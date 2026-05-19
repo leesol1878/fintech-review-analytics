839677ef7d054c46b7ef11f11de02d1af0371f3a
leesol1878 <stsionastw@gmail.com>
2026-05-19 22:04:27 +0300

    test: add requirements.txt existence test

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,6 +1,10 @@
 def test_always_passes():
     assert True == True
 
-def test_project_exists():
+def test_readme_exists():
     import os
     assert os.path.exists('README.md')
+
+def test_requirements_exists():
+    import os
+    assert os.path.exists('requirements.txt')
